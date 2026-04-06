847bef6c707b56a7995946a12929acaab264800a
Ali Shan <alishansadaqatali@gmail.com>
2026-04-06 13:19:56 +0500

    Added component with header footer

--- app/page.js
+++ app/page.js
@@ -1,21 +1,16 @@
-import Navbar from "@/components/Navbar"; // Navbar ko bulaya
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer"; // 1. Footer ko bulaya
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-100">
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
       
-      {/* Ab sirf ek line likhni hai */}
-      <Navbar /> 
-
       <main className="flex-grow flex items-center justify-center">
-        <div className="text-center">
-          <h2 className="text-4xl font-extrabold text-gray-900">
-            I am a Developer
-          </h2>
-          <p className="text-gray-500 mt-2">Main Next.js seekh raha hoon.</p>
-        </div>
+        <h1 className="text-3xl font-bold">Foundation Mazboot Ho Rahi Hai!</h1>
       </main>
 
+      <Footer /> {/* 2. Footer ko yahan rakha */}
     </div>
   );
 }
