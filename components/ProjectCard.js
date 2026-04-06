75672a8ca1a5258710d5c806952219e1247fba89
Ali Shan <alishansadaqatali@gmail.com>
2026-04-06 13:58:03 +0500

    Added projects props color

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -4,7 +4,7 @@ export default function ProjectCard({ title, desc }) {
       <div className="bg-white p-6 rounded-xl shadow-md border-t-4 border-blue-500">
         <h2 className="text-xl font-bold text-gray-800">{title}</h2>
         <p className="text-gray-600 mt-2">{desc}</p>
-        <button className="mt-4 text-blue-600 font-bold hover:underline">
+        <button className="mt-4 text-red-600 font-bold hover:underline">
           Read More →
         </button>
       </div>
