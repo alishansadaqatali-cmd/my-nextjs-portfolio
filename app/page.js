bfe5b3693d87d64b23ec8514bd75eecb9960dca5
Ali Shan <alishansadaqatali@gmail.com>
2026-04-06 12:33:09 +0500

    Added a new line

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
       <h1 className="text-5xl font-bold">
         Bismillah! My Next.js Journey
       </h1>
+      <p className="mt-4 text-gray-400">This is my first live update!</p>
     </main>
   );
 }
