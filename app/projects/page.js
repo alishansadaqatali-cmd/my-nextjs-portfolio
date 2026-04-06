1b8f1a69977534ed6edc3902bd9c0db2f7f55943
Ali Shan <alishansadaqatali@gmail.com>
2026-04-06 13:41:56 +0500

    Added projects  page and Navigation linksss

--- app/projects/page.js
+++ app/projects/page.js
@@ -6,7 +6,7 @@ export default function ProjectsPage() {
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <main className="flex-grow p-20">
-        <h1 className="text-4xl font-bold text-blue-600">About Me</h1>
+        <h1 className="text-4xl font-bold text-blue-600">ProjectsPage Me</h1>
         <p className="mt-4 text-gray-600 text-xl">
           yeh prject page bna h jo m n practice j toor pr kiya h
         </p>
