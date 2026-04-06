6dc29f991c2f60ea7618024f29202d6270197eab
Ali Shan <alishansadaqatali@gmail.com>
2026-04-06 12:54:26 +0500

    Added styled card with Tailwindd

--- app/page.js
+++ app/page.js
@@ -1,17 +1,24 @@
-export default function home(){
-  return(
+export default function Home() {
+  return (
     <div className="bg-slate-900 h-screen flex flex-col items-center justify-center p-4">
-      <div>
-        <h1 className="bg-white p-10 rounded-2xl shadow-2xl text-center max-w-md">
-        Tailwind is Awesome!
+      
+      {/* Yeh Main White Box Hai */}
+      <div className="bg-white p-10 rounded-3xl shadow-2xl text-center max-w-md">
+        
+        <h1 className="text-3xl font-black text-blue-600 mb-4">
+          Tailwind is Awesome!
         </h1>
-        <p className="text-gray-600 text-lg">
-        Main Next.js seekh raha hoon aur ye mera pehla styled component hai.
+        
+        <p className="text-gray-500 text-lg">
+          Main Next.js seekh raha hoon aur ye mera pehla styled component hai.
         </p>
-        <button className="mt-6 bg-blue-600 text-white px-8 py-3 rounded-full font-bold hover:bg-blue-700 transition-all">
-        Click Me!
+        
+        <button className="mt-6 bg-blue-600 text-white px-8 py-3 rounded-xl font-bold hover:bg-blue-800 transition-all shadow-lg">
+          Click Me!
         </button>
+
       </div>
+
     </div>
   );
 }
