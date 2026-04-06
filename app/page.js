863c71e8e3262c16259731f90316008022c1572b
Ali Shan <alishansadaqatali@gmail.com>
2026-04-06 12:50:39 +0500

    Added styled card with Tailwind

--- app/page.js
+++ app/page.js
@@ -1,10 +1,17 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-black text-white">
-      <h1 className="text-5xl font-bold">
-        Bismillah! My Next.js Journey
-      </h1>
-      <p className="mt-4 text-gray-400">This is my first live update!</p>
-    </main>
+export default function home(){
+  return(
+    <div className="bg-slate-900 h-screen flex flex-col items-center justify-center p-4">
+      <div>
+        <h1 className="bg-white p-10 rounded-2xl shadow-2xl text-center max-w-md">
+        Tailwind is Awesome!
+        </h1>
+        <p className="text-gray-600 text-lg">
+        Main Next.js seekh raha hoon aur ye mera pehla styled component hai.
+        </p>
+        <button className="mt-6 bg-blue-600 text-white px-8 py-3 rounded-full font-bold hover:bg-blue-700 transition-all">
+        Click Me!
+        </button>
+      </div>
+    </div>
   );
 }
