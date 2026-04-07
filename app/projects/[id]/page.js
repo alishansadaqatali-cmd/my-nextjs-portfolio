c601217d51e8a9831ce92b77a78667ece18f0000
Ali Shan <alishansadaqatali@gmail.com>
2026-04-07 13:06:11 +0500

    Added styled card with routes

--- app/projects/[id]/page.js
+++ app/projects/[id]/page.js
@@ -1,10 +1,18 @@
-export default function ProjectDetail({ params }) {
-    // params.id mein wo ID ayegi jo URL mein hogi (e.g., /projects/1)
+// 1. Function ko "async" banayein
+export default async function ProjectDetail({ params }) {
+  
+    // 2. Params ko "await" karein (Naye Next.js ka rule hai)
+    const { id } = await params;
+  
     return (
       <div className="p-20">
         <h1 className="text-4xl font-bold">Project Detail Page</h1>
         <div className="mt-10 p-10 bg-blue-100 rounded-lg">
-          <h2 className="text-2xl">Aap abhi Project Number: <span className="font-black text-blue-600">{params.id}</span> dekh rahe hain.</h2>
+          <h2 className="text-2xl">
+            Aap abhi Project Number: 
+            <span className="font-black text-blue-600 ml-2">{id}</span> 
+            dekh rahe hain.
+          </h2>
           <p className="mt-4">InshaAllah yahan hum Strapi se project ka sara data lekar ayenge!</p>
         </div>
       </div>
